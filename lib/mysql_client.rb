be39aa2865a6131ad9ed933044db884408e7355a
Leonis0813 <Leonis.0813@gmail.com>
2018-01-12 23:18:56 +0000

    add log to mysql client

--- lib/mysql_client.rb
+++ lib/mysql_client.rb
@@ -7,6 +7,11 @@ class MysqlClient
     values.map! {|value| "'#{value}'" }
     client.query("INSERT IGNORE INTO #{table} VALUES (NULL,#{values.join(',')})")
     id = client.last_id
+    if id == 0
+      Logger.info(:action => 'insert', :table => table, :message => 'already_exist')
+    else
+      Logger.info(:action => 'insert', :table => table, :id => id, :values => values)
+    end
     client.close
     id
   end
