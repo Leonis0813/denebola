6c8c7eac93d708d32c6044e7dc1e8796d49defd1
Leonis0813 <Leonis.0813@gmail.com>
2018-01-12 19:38:08 +0000

    create collect.rb

--- collect.rb
+++ collect.rb
@@ -1,14 +1,18 @@
 # coding: utf-8
-require_relative 'collect/fetch'
+require_relative 'config/settings'
+require_relative 'lib/http_client'
+Dir['models/*'].each {|f| require_relative f }
 
 from = ARGV[0] ? Date.parse(ARGV[0]) : (Date.today - 7)
 to = ARGV[1] ? Date.parse(ARGV[1]) : Date.today
+client = HTTPClient.new
 
 (from..to).each do |date|
-  fetch(:race_list, date.strftime('%Y%m%d')).each do |race_id|
-    race = fetch(:race, race_id)
-    insert(:race, race)
-    race.entries.each {|entry| insert(entry) }
-    race.results.each {|result| insert(entry.result) }
+  response = client.get("#{Settings.url}#{Settings.path['race_list']}/#{date.strftime('%Y%m%d')}")
+  race_ids = response.body.scan(%r[.*/race/(\d+)]).flatten
+
+  race_ids.each do |race_id|
+    response = client.get("#{Settings.url}#{Settings.path['race']}/#{race_id}")
+    Race.new(response.body).save!
   end
 end
