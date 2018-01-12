69f7106fadec131734aec3aa52c6070458917415
Leonis0813 <Leonis.0813@gmail.com>
2018-01-12 19:42:17 +0000

    update library

--- lib/mysql_client.rb
+++ lib/mysql_client.rb
@@ -1,51 +1,11 @@
 require 'mysql2'
+require_relative '../config/settings'
 
-def get_race_id(race_name, start_time, place)
-  client = Mysql2::Client.new(Settings.mysql)
-  query =<<"EOF"
-SELECT
-  id
-FROM
-  races
-WHERE
-  name = '#{race_name}'
-  AND start_time = '#{start_time}'
-  AND place = '#{place}'
-LIMIT 1
-EOF
-  begin
-    result = client.query(query)
-    client.close
-    result.first['id']
-  rescue Mysql2::Error => e
-    Logger.error(:action => 'get_race_id', :race_name => race_name, :start_time => start_time, :place => place, :message => e.message)
-    raise
-  ensure
-    client.close
-  end
-end
-
-def get_horse_id(external_id)
-  client = Mysql2::Client.new(Settings.mysql)
-  query =<<"EOF"
-SELECT
-  id
-FROM
-  horses
-WHERE
-  external_id = #{external_id}
-ORDER BY
-  birthday desc
-LIMIT 1
-EOF
-  begin
-    result = client.query(query)
-    client.close
-    result.first['id']
-  rescue Mysql2::Error => e
-    Logger.error(:action => 'get_horse_id', :external_id => external_id, :message => e.message)
-    raise
-  ensure
+class MysqlClient
+  def execute_query(query, params = {})
+    client = Mysql2::Client.new(Settings.mysql)
+    params.each {|key, value| query.gsub!("{{#{key}}}", value) }
+    client.query(query)
     client.close
   end
 end
