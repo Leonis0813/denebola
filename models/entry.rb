935766d378b67b5ffc31e92afb5143c20b87e070
Leonis0813 <Leonis.0813@gmail.com>
2018-03-11 10:56:51 +0000

    create models of activerecord

--- models/entry.rb
+++ models/entry.rb
@@ -1,43 +1,5 @@
 # coding: utf-8
-require_relative '../lib/mysql_client'
-
-class Entry
-  attr_accessor :number, :age, :burden_weight, :weight, :race_id
-  attr_accessor :result
-
-  def initialize(html)
-    attributes = html.gsub(/<[\/]?tr>/, '').scan(/<td.*?>(.*?)<\/td>/).flatten
-    attributes.map! {|attribute| attribute.gsub(/<.*?>/, '') }
-
-    @number = attributes[2].to_i
-    @age = attributes[4].match(/(\d+)\z/)[1].to_i
-    @burden_weight = attributes[5].to_f
-    @weight = attributes[14] == '計不' ? 'NULL' : features[14].match(/\A(\d+)/)[1].to_f rescue 'NULL'
-    @result = Result.new(attributes)
-  end
-
-  def save!
-    client = MysqlClient.new
-    id = Logger.write_with_runtime(:action => 'insert', :resource => 'entry', :params => attributes) do
-      client.insert(:entries, [@number, @age, @burden_weight, @weight, @race_id])
-    end
-
-    @result.race_id = @race_id
-    @result.entry_id = id
-    @result.save!
-  end
-
-  def self.find(id)
-    client = MysqlClient.new
-    client.select(['*'], :entries, "id = #{id}")
-  end
-
-  def attributes
-    {
-      :number => @number,
-      :age => @age,
-      :burden_weight => @burden_weight,
-      :weight => @weight,
-    }
-  end
+class Entry < ActiveRecord::Base
+  belongs_to :race
+  has_one :result
 end
--- models/feature.rb
+++ models/feature.rb
@@ -1,22 +1,5 @@
-require_relative '../lib/mysql_client'
-
-class Feature
-  attr_accessor :track, :direction, :distance, :weather, :place, :round,
-                :number, :age, :burden_weight, :weight,
-                :order,
-                :race_id, :entry_id
-
+class Feature < ActiveRecord::Base
   def initialize(attributes)
     attributes.each {|key, value| self.send("#{key}=", value) }
-  end
-
-  def save!
-    client = MysqlClient.new
-    client.insert(:features, ['NULL'] * 11 + [@race_id, @entry_id])
-  end
-
-  def update!(attributes)
-    client = MysqlClient.new
-    client.update(:features, attributes, "race_id = #{@race_id} AND entry_id = #{@entry_id}")
   end
 end
