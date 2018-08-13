7a0718e9d72b2cf172c46d5e8e2b17b806f66a83
Leonis0813 <Leonis.0813@gmail.com>
2018-08-13 05:39:10 +0100

    add to aggregate

--- aggregate.rb
+++ aggregate.rb
@@ -3,10 +3,20 @@ require_relative 'config/initialize'
 require_relative 'db/connect'
 Dir['models/*'].each {|f| require_relative f }
 
+logger = Logger.new('log/aggregate.log')
+logger.formatter = proc do |severity, datetime, progname, message|
+  time = datetime.utc.strftime(Settings.logger.time_format)
+  "[#{severity}] [#{time}]: #{message}\n"
+end
+
+logger.info('Start Aggregation')
+
 diffs = Result.pluck(:race_id, :entry_id).uniq - Feature.pluck(:race_id, :entry_id).uniq
 updated_features = diffs.map do |race_id, entry_id|
   Feature.new(:race_id => race_id, :entry_id => entry_id)
 end
+
+logger.info("# of Updated Features = #{updated_features.size}")
 
 updated_features.each do |feature|
   race = Race.find(feature.race_id)
@@ -18,4 +28,8 @@ updated_features.each do |feature|
   result = Result.find_by(:race_id => feature.race_id, :entry_id => feature.entry_id)
   feature_attributes.merge!(result.attributes.slice(*Feature.attribute_names))
   feature.update!(feature_attributes.except('id', 'created_at', 'updated_at'))
+
+  logger.info(feature_attributes.except('race_id', 'entry_id', 'created_at', 'updated_at'))
 end
+
+logger.info('Finish Aggregation')
