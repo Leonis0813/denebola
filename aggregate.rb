4afbb47bb585b7ee094a783bcd5fd96e8ab31d24
Leonis <Leonis.0813@gmail.com>
2018-09-14 21:23:59 +0900

    Fix bug (#13)
    * modify logger
    
    * save => create

--- aggregate.rb
+++ aggregate.rb
@@ -6,9 +6,9 @@ Dir['models/*'].each {|f| require_relative f }
 logger = Logger.new('log/aggregate.log')
 logger.formatter = proc do |severity, datetime, progname, message|
   time = datetime.utc.strftime(Settings.logger.time_format)
-  log = "[#{severity}] [#{time}]: #{message}\n"
-  p log if ENV['STDOUT'] == 'on'
-  log
+  log = "[#{severity}] [#{time}]: #{message}"
+  puts log if ENV['STDOUT'] == 'on'
+  "#{log}\n"
 end
 
 logger.info('Start Aggregation')
@@ -18,19 +18,20 @@ new_features = Result.pluck(:race_id, :entry_id).uniq - Feature.pluck(:race_id, 
 logger.info("# of Updated Features = #{new_features.size}")
 
 new_features.each do |race_id, entry_id|
-  feature = Feature.new(:race_id => race_id, :entry_id => entry_id)
+  attribute = {:race_id => race_id, :entry_id => entry_id}
 
-  race = Race.find(feature.race_id)
-  feature_attributes = race.attributes.slice(*Feature.attribute_names)
+  race = Race.find(race_id)
+  attribute.merge!(race.attributes.slice(*Feature.attribute_names))
 
-  entry = Entry.find(feature.entry_id)
-  feature_attributes.merge!(entry.attributes.slice(*Feature.attribute_names))
+  entry = Entry.find(entry_id)
+  attribute.merge!(entry.attributes.slice(*Feature.attribute_names))
 
-  result = Result.find_by(:race_id => feature.race_id, :entry_id => feature.entry_id)
-  feature_attributes.merge!(result.attributes.slice(*Feature.attribute_names))
-  feature.save!(feature_attributes.except('id', 'created_at', 'updated_at'))
+  result = Result.find_by(:race_id => race_id, :entry_id => entry_id)
+  attribute.merge!(result.attributes.slice(*Feature.attribute_names))
 
-  logger.info(feature_attributes.except('race_id', 'entry_id', 'created_at', 'updated_at'))
+  Feature.create!(attribute)
+
+  logger.info(attribute)
 end
 
 logger.info('Finish Aggregation')
