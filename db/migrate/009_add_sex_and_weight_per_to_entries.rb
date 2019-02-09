e722028183f21b48c795992d9aaebc9d5992944c
Leonis0813 <Leonis.0813@gmail.com>
2019-02-09 10:27:27 +0000

    remove weight_per from entries

--- db/migrate/009_add_sex_and_weight_per_to_entries.rb
+++
@@ -1,6 +0,0 @@
-class AddSexAndWeightPerToEntries < ActiveRecord::Migration
-  def change
-    add_column :entries, :sex, :string, :null => false, :after => :age
-    add_column :entries, :weight_per, :float, :after => :weight_diff
-  end
-end
