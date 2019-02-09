6a20fc499039bd3bf88bfcca9cd887412399960c
Leonis0813 <Leonis.0813@gmail.com>
2019-02-09 10:17:34 +0000

    month to integer

--- db/migrate/008_add_month_and_sex_and_weight_per_to_features.rb
+++ db/migrate/008_add_month_and_sex_and_weight_per_to_features.rb
@@ -1,6 +1,6 @@
 class AddMonthAndSexAndWeightPerToFeatures < ActiveRecord::Migration
   def change
-    add_column :features, :month, :string, :null => false, :after => :grade
+    add_column :features, :month, :integer, :null => false, :after => :grade
     add_column :features, :sex, :string, :null => false, :after => :age
     add_column :features, :weight_per, :float, :after => :weight_diff
   end
