1aed89c714f23ce2e5d5470ac11e4e78478730f9
Leonis <Leonis.0813@gmail.com>
2019-02-09 18:04:28 +0900

    Introduce html parser (#25)
    * install nokogiri
    
    * for https
    
    * apply nokogiri
    
    * update CHANGELOG.md

--- lib/http_client.rb
+++ lib/http_client.rb
@@ -3,7 +3,7 @@ require 'net/http'
 class HTTPClient
   def get(url)
     parsed_url = URI.parse(url)
-    Net::HTTP.start(parsed_url.host, parsed_url.port) do |http|
+    Net::HTTP.start(parsed_url.host, parsed_url.port, :use_ssl => true) do |http|
       http.request Net::HTTP::Get.new(parsed_url)
     end
   end
