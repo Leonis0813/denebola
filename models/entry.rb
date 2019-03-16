0467da55f9ae87c4750e2a803ec697a70bf0a0fe
Leonis0813 <Leonis.0813@gmail.com>
2019-03-16 05:14:30 +0000

    remove result model

--- models/entry.rb
+++ models/entry.rb
@@ -1,5 +1,4 @@
 class Entry < ActiveRecord::Base
   belongs_to :race
   has_one :horse
-  has_one :result
 end
--- models/race.rb
+++ models/race.rb
@@ -1,5 +1,4 @@
 # coding: utf-8
 class Race < ActiveRecord::Base
   has_many :entries
-  has_many :results, :through => :entry
 end
