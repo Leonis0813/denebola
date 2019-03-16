64018cb47fbb7644e09f3aa26686c7054d9f194c
Leonis <Leonis.0813@gmail.com>
2019-03-16 16:35:59 +0900

    Refactor flow (#32)
    * fetch only
    
    * create script to extract
    
    * refactor extarct scripts
    
    * remove result model
    
    * modify db schema
    
    * fix bug
    
    * remove result from aggregate.rb
    
    * update CHANGELOG.md

--- collect.rb
+++ collect.rb
@@ -2,9 +2,7 @@
 require 'logger'
 require 'nokogiri'
 require_relative 'config/initialize'
-require_relative 'db/connect'
 require_relative 'lib/http_client'
-Dir['models/*'].each {|f| require_relative f }
 
 BACKUP_DIR = File.join(APPLICATION_ROOT, 'backup')
 
@@ -14,48 +12,6 @@ logger.formatter = proc do |severity, datetime, progname, message|
   log = "[#{severity}] [#{time}]: #{message}"
   puts log if ENV['STDOUT'] == 'on'
   "#{log}\n"
-end
-
-def extract_race_info(html)
-  {}.tap do |feature|
-    race_data = html.xpath('//dl[contains(@class, "racedata")]')
-
-    track, weather, _, start_time, _ = race_data.search('span').text.split('/')
-    feature[:track] = track[0].sub('ダ', 'ダート')
-    feature[:direction] = track[1]
-    feature[:distance] = track.match(/(\d*)m/)[1].to_i
-    feature[:weather] = weather.match(/天候 : (.*)/)[1]
-    feature[:grade] = race_data.search('h1').text.match(/\((.*)\)$/).try(:[], 1)
-    feature[:place] = html.xpath('//ul[contains(@class, "race_place")]').first
-                      .children[1].text.match(/<a.*class="active">(.*?)<\/a>/)[1]
-    feature[:round] = race_data.search('dt').text.strip.match(/^(\d*) R$/)[1].to_i
-
-    race_date = html.xpath('//li[@class="result_link"]').text.match(/(\d*年\d*月\d*日)のレース結果/)[1]
-    race_date = race_date.gsub(/年|月/, '-').sub('日', '')
-    start_time = start_time.match(/発走 : (.*)/)[1]
-    feature[:start_time] = "#{race_date} #{start_time}:00"
-
-    _, *data = race_data.xpath('//table[contains(@class, "race_table")]').search('tr')
-    feature[:entries] = data.map do |entry|
-      attributes = entry.search('td').map(&:text).map(&:strip)
-      horse_link = entry.search('td')[3].first_element_child.attribute('href').value
-      {
-        :age => attributes[4].match(/(\d+)\z/)[1].to_i,
-        :sex => attributes[4].match(/\A(.*)\d+/)[1],
-        :burden_weight => attributes[5].to_f,
-        :number => attributes[2].to_i,
-        :weight => attributes[14].match(/\A(\d+)/).try(:[], 1).to_f,
-        :weight_diff => attributes[14].match(/\((.+)\)$/).try(:[], 1).to_f,
-        :jockey => attributes[6],
-        :result => {:order => attributes[0]},
-        :horse_id => horse_link.match(/\/horse\/(?<horse_id>\d+)\/?/)[:horse_id]
-      }
-    end
-  end
-end
-
-def extract_horse_info(html)
-  {}
 end
 
 begin
@@ -76,65 +32,46 @@ Settings.backup_dir.to_h.values.each {|path| FileUtils.mkdir_p(File.join(BACKUP_
   date = date.strftime('%Y%m%d')
 
   file_path = File.join(BACKUP_DIR, Settings.backup_dir.race_list, "#{date}.txt")
-  base_log_body = {:action => 'fetch', :resource => 'race_list'}
   race_ids = if File.exists?(file_path)
                ids = File.read(file_path).split("\n")
-               logger.info(base_log_body.merge(:file_path => File.basename(file_path)))
+               logger.info(:file_path => File.basename(file_path), :race_ids => ids)
                ids
              else
                res = client.get("#{Settings.url}#{Settings.path.race_list}/#{date}")
-               logger.info(base_log_body.merge(:uri => res.uri.to_s, :status => res.code))
                ids = res.body.scan(%r[.*/race/(\d+)]).flatten
+               logger.info(:uri => res.uri.to_s, :status => res.code, :race_ids => ids)
                File.open(file_path, 'w') {|out| out.write(ids.join("\n")) }
                ids
              end
 
   race_ids.each do |race_id|
     file_path = File.join(BACKUP_DIR, Settings.backup_dir.race, "#{race_id}.html")
-    base_log_body = {:action => 'fetch', :resource => 'race'}
     html = if File.exists?(file_path)
              html = File.read(file_path)
-             logger.info(base_log_body.merge(:file_path => File.basename(file_path)))
+             logger.info(:resource => 'race', :file_path => File.basename(file_path))
              html
            else
              res = client.get("#{Settings.url}#{Settings.path.race}/#{race_id}")
-             logger.info(base_log_body.merge(:uri => res.uri.to_s, :status => res.code))
-             body = res.body.encode('utf-8', 'euc-jp', :undef => :replace, :replace => '?')
-             File.open(file_path, 'w') {|out| out.write(body) }
-             body
+             logger.info(:resource => 'race', :uri => res.uri.to_s, :status => res.code)
+             html = res.body.encode('utf-8', 'euc-jp', :undef => :replace, :replace => '?')
+             html.gsub!('&nbsp;', ' ')
+             File.open(file_path, 'w') {|out| out.write(html) }
+             html
            end
 
-    parsed_html = Nokogiri::HTML.parse(html.gsub('&nbsp;', ' '))
-    race_info = extract_race_info(parsed_html)
-    next unless race_info
+    parsed_html = Nokogiri::HTML.parse(html)
+    _, *entries = parsed_html.xpath('//table[contains(@class, "race_table")]').search('tr')
 
-    race = Race.find_or_create_by!(race_info.except(:entries))
-    logger.info(:action => 'create', :resource => 'race', :id => race.id)
-    race_info[:entries].each do |entry|
-      e = race.entries.find_or_create_by!(entry.except(:result, :horse_id))
-      logger.info(:action => 'create', :resource => 'entry', :id => e.id)
+    entries.each do |entry|
+      horse_link = entry.search('td')[3].first_element_child.attribute('href').value
+      horse_id = horse_link.match(/\/horse\/(?<horse_id>\d+)\/?/)[:horse_id]
 
-      e.result = Result.find_or_create_by!(entry[:result].merge(:race_id => race.id))
-      logger.info(:action => 'create', :resource => 'result', :id => e.result.id)
-
-      unless Horse.exists?(:horse_id => entry[:horse_id])
-        file_path = File.join(BACKUP_DIR, Settings.backup_dir.horse, "#{entry[:horse_id]}.html")
-        base_log_body = {:action => 'fetch', :resource => 'horse'}
-        html = if File.exists?(file_path)
-                 html = File.read(file_path)
-                 logger.info(base_log_body.merge(:file_path => File.basename(file_path)))
-                 html
-               else
-                 res = client.get("#{Settings.url}#{Settings.path.horse}/#{entry[:horse_id]}")
-                 logger.info(base_log_body.merge(:uri => res.uri.to_s, :status => res.code))
-                 body = res.body.encode('utf-8', 'euc-jp', :undef => :replace, :replace => '?')
-                 File.open(file_path, 'w') {|out| out.write(body) }
-                 body
-               end
-
-        parsed_html = Nokogiri::HTML.parse(html.gsub('&nbsp;', ' '))
-        horse_info = extract_horse_info(parsed_html)
-        horse = Horse.create!(horse_info.merge(:horse_id => entry[:horse_id])) if horse_info
+      file_path = File.join(BACKUP_DIR, Settings.backup_dir.horse, "#{horse_id}.html")
+      unless File.exists?(file_path)
+        res = client.get("#{Settings.url}#{Settings.path.horse}/#{horse_id}")
+        logger.info(:resource => 'horse', :uri => res.uri.to_s, :status => res.code)
+        html = res.body.encode('utf-8', 'euc-jp', :undef => :replace, :replace => '?')
+        File.open(file_path, 'w') {|out| out.write(html.gsub('&nbsp;', ' ')) }
       end
     end
   end
---
+++ extract.rb
@@ -0,0 +1,71 @@
+# coding: utf-8
+require 'logger'
+require 'nokogiri'
+require_relative 'config/initialize'
+require_relative 'db/connect'
+Dir['extract/*'].each {|f| require_relative f }
+Dir['models/*'].each {|f| require_relative f }
+
+BACKUP_DIR = File.join(APPLICATION_ROOT, 'backup')
+
+logger = Logger.new('log/extract.log')
+logger.formatter = proc do |severity, datetime, progname, message|
+  time = datetime.utc.strftime(Settings.logger.time_format)
+  log = "[#{severity}] [#{time}]: #{message}"
+  puts log if ENV['STDOUT'] == 'on'
+  "#{log}\n"
+end
+
+begin
+  from = ARGV.find {|arg| arg.start_with?('--from=') }
+  from = from ? Date.parse(from.match(/\A--from=(.*)$\z/)[1]) : (Date.today - 7)
+  to = ARGV.find {|arg| arg.start_with?('--to=') }
+  to = to ? Date.parse(to.match(/\A--to=(.*)\z/)[1]) : Date.today
+rescue Exception => e
+  logger.error(e.backtrace.join("\n"))
+  raise e
+end
+
+(from..to).each do |date|
+  date = date.strftime('%Y%m%d')
+
+  file_path = File.join(BACKUP_DIR, Settings.backup_dir.race_list, "#{date}.txt")
+  next unless File.exists?(file_path)
+  race_ids = File.read(file_path).split("\n")
+  logger.info(:action => 'read', :file_path => File.basename(file_path), :ids => race_ids)
+
+  race_ids.each do |race_id|
+    file_path = File.join(BACKUP_DIR, Settings.backup_dir.race, "#{race_id}.html")
+    next unless File.exists?(file_path)
+    html = File.read(file_path)
+    logger.info(:action => 'read', :resource => 'race', :file_path => File.basename(file_path))
+
+    parsed_html = Nokogiri::HTML.parse(html)
+    race_attribute = extract_race(parsed_html)
+    next unless race_attribute
+
+    race = Race.find_or_create_by!(race_attribute)
+    logger.info(:action => 'create', :resource => 'race', :race_id => race.id)
+
+    _, *rows = parsed_html.xpath('//table[contains(@class, "race_table")]').search('tr')
+    rows.each do |row|
+      entry_attribute = extract_entry(row)
+      entry = race.entries.find_or_create_by!(entry_attribute.except(:horse_id))
+      logger.info(:action => 'create', :resource => 'entry', :entry_id => entry.id)
+
+      horse_id = entry_attribute[:horse_id]
+      unless Horse.exists?(:horse_id => horse_id)
+        file_path = File.join(BACKUP_DIR, Settings.backup_dir.horse, "#{horse_id}.html")
+        html = File.read(file_path) unless File.exists?(file_path)
+        logger.info(:action => 'read', :resource => 'horse', :file_path => File.basename(file_path))
+
+        parsed_html = Nokogiri::HTML.parse(html)
+        horse_attribute = extract_horse(parsed_html)
+        if horse_attribute
+          Horse.create!(horse_attribute.merge(:horse_id => horse_id))
+          logger.info(:action => 'create', :resource => 'horse', :horse_id => horse_id)
+        end
+      end
+    end
+  end
+end
