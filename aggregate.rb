604a1963093f7fb7f2f6414ef997140a69e97e06
Leonis0813 <Leonis.0813@gmail.com>
2019-04-15 11:23:17 +0000

    apply denebola logger for aggregate.rb

--- aggregate.rb
+++ aggregate.rb
@@ -1,16 +1,9 @@
-require 'logger'
 require_relative 'config/initialize'
 require_relative 'db/connect'
+require_relative 'lib/denebola_logger'
 Dir['models/*'].each {|f| require_relative f }
 
-logger = Logger.new('log/aggregate.log')
-logger.formatter = proc do |severity, datetime, progname, message|
-  time = datetime.utc.strftime(Settings.logger.time_format)
-  log = "[#{severity}] [#{time}]: #{message}"
-  puts log if ENV['STDOUT'] == 'on'
-  "#{log}\n"
-end
-
+logger = Logger.new(Settings.logger.path.aggregate)
 logger.info('Start Aggregation')
 
 new_features = Entry.pluck(:race_id, :id).uniq - Feature.pluck(:race_id, :entry_id).uniq
