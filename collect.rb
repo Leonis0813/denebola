46dae3eb6c553f1ae5d00e55a57cdb0da8cfad24
Leonis0813 <Leonis.0813@gmail.com>
2019-04-15 12:35:07 +0000

    fix bug

--- collect.rb
+++ collect.rb
@@ -34,9 +34,10 @@ end
                logger.info(:source => 'file', :file_path => File.basename(file_path), :race_ids => ids)
                ids
              else
-               res = client.get("#{Settings.url}#{Settings.path.race_list}/#{date}")
+               uri = "#{Settings.url}#{Settings.path.race_list}/#{date}"
+               res = client.get(uri)
                ids = res.body.scan(%r[.*/race/(\d+)]).flatten
-               logger.info(:source => 'web', :uri => res.uri.to_s, :status => res.code, :race_ids => ids)
+               logger.info(:source => 'web', :uri => uri, :status => res.code, :race_ids => ids)
                File.open(file_path, 'w') {|out| out.write(ids.join("\n")) }
                ids
              end
@@ -48,8 +49,9 @@ end
              logger.info(:resource => 'race', :source => 'file', :file_path => File.basename(file_path))
              html
            else
-             res = client.get("#{Settings.url}#{Settings.path.race}/#{race_id}")
-             logger.info(:resource => 'race', :souroce => 'web', :uri => res.uri.to_s, :status => res.code)
+             uri = "#{Settings.url}#{Settings.path.race}/#{race_id}"
+             res = client.get(uri)
+             logger.info(:resource => 'race', :souroce => 'web', :uri => uri, :status => res.code)
              html = res.body.encode('utf-8', 'euc-jp', :undef => :replace, :replace => '?')
              html.gsub!('&nbsp;', ' ')
              File.open(file_path, 'w') {|out| out.write(html) }
@@ -65,8 +67,9 @@ end
 
       file_path = File.join(BACKUP_DIR, Settings.backup_dir.horse, "#{horse_id}.html")
       unless File.exists?(file_path)
-        res = client.get("#{Settings.url}#{Settings.path.horse}/#{horse_id}")
-        logger.info(:resource => 'horse', :source => 'web', :uri => res.uri.to_s, :status => res.code)
+        uri = "#{Settings.url}#{Settings.path.horse}/#{horse_id}"
+        res = client.get(uri)
+        logger.info(:resource => 'horse', :source => 'web', :uri => uri, :status => res.code)
         html = res.body.encode('utf-8', 'euc-jp', :undef => :replace, :replace => '?')
         File.open(file_path, 'w') {|out| out.write(html.gsub('&nbsp;', ' ')) }
       end
