7efaffaf1e8e06b2eba5055b6f1caefc288d515f
Leonis0813 <Leonis.0813@gmail.com>
2019-04-21 06:17:55 +0000

    fix aggregation bug

--- aggregate.rb
+++ aggregate.rb
@@ -3,7 +3,7 @@ require_relative 'db/connect'
 require_relative 'lib/denebola_logger'
 Dir['models/*'].each {|f| require_relative f }
 
-logger = Logger.new(Settings.logger.path.aggregate)
+logger = DenebolaLogger.new(Settings.logger.path.aggregate)
 logger.info('Start Aggregation')
 
 new_features = Entry.pluck(:race_id, :id).uniq - Feature.pluck(:race_id, :entry_id).uniq
@@ -14,22 +14,22 @@ new_features.each do |race_id, entry_id|
   attribute = {:race_id => race_id, :entry_id => entry_id}
 
   race = Race.find(race_id)
-  attribute.merge!(race.attributes.slice(*Feature.attribute_names))
+  attribute.merge!(race.attributes.slice(*Feature.attribute_names)).symbolize_keys!
 
   entry = Entry.find(entry_id)
-  attribute.merge!(entry.attributes.slice(*Feature.attribute_names))
+  attribute.merge!(entry.attributes.slice(*Feature.attribute_names)).symbolize_keys!
 
   horse = Horse.find(entry.horse_id)
-  attribute.merge!(horse.attributes.slice(*Feature.attribute_names))
+  attribute.merge!(horse.attributes.slice(*Feature.attribute_names)).symbolize_keys!
 
   weight_per = if entry.burden_weight.to_i > 0 and entry.weight.to_i > 0
                  entry.burden_weight / entry.weight
                end
   attribute.merge!(:month => race.start_time.month, :weight_per => weight_per)
 
-  Feature.create!(attribute)
+  feature = Feature.create!(attribute.except(:id))
 
-  logger.info(attribute.slice(:race_id, :entry_id))
+  logger.info(:action => 'create', :resource => 'feature', :feature_id => feature.id)
 end
 
 logger.info('Finish Aggregation')
