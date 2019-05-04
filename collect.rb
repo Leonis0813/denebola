e2aea197efbb8dbb5dac64b2305d3a45aca4d86f
Leonis0813 <Leonis.0813@gmail.com>
2019-05-04 05:17:39 +0000

    fix Style/IdenticalConditionalBranches

--- collect.rb
+++ collect.rb
@@ -30,32 +30,32 @@ end
 
   file_path = File.join(BACKUP_DIR, Settings.backup_dir.race_list, "#{date}.txt")
   race_ids = if File.exists?(file_path)
-               ids = File.read(file_path).split("\n")
-               logger.info(source: 'file', file_path: File.basename(file_path), race_ids: ids)
-               ids
+               ids_from_file = File.read(file_path).split("\n")
+               logger.info(source: 'file', file_path: File.basename(file_path), race_ids: ids_from_file)
+               ids_from_file
              else
                uri = "#{Settings.url}#{Settings.path.race_list}/#{date}"
                res = client.get(uri)
-               ids = res.body.scan(%r[.*/race/(\d+)]).flatten
-               logger.info(source: 'web', uri: uri, status: res.code, race_ids: ids)
-               File.open(file_path, 'w') {|out| out.write(ids.join("\n")) }
-               ids
+               ids_from_remote = res.body.scan(%r[.*/race/(\d+)]).flatten
+               logger.info(source: 'web', uri: uri, status: res.code, race_ids: ids_from_remote)
+               File.open(file_path, 'w') {|out| out.write(ids_from_remote.join("\n")) }
+               ids_from_remote
              end
 
   race_ids.each do |race_id|
     file_path = File.join(BACKUP_DIR, Settings.backup_dir.race, "#{race_id}.html")
     html = if File.exists?(file_path)
-             html = File.read(file_path)
+             html_from_file = File.read(file_path)
              logger.info(resource: 'race', source: 'file', file_path: File.basename(file_path))
-             html
+             html_from_file
            else
              uri = "#{Settings.url}#{Settings.path.race}/#{race_id}"
              res = client.get(uri)
              logger.info(resource: 'race', souroce: 'web', uri: uri, status: res.code)
-             html = res.body.encode('utf-8', 'euc-jp', undef: :replace, replace: '?')
-             html.gsub!('&nbsp;', ' ')
-             File.open(file_path, 'w') {|out| out.write(html) }
-             html
+             html_from_remote = res.body.encode('utf-8', 'euc-jp', undef: :replace, replace: '?')
+             html_from_remote.gsub!('&nbsp;', ' ')
+             File.open(file_path, 'w') {|out| out.write(html_from_remote) }
+             html_from_remote
            end
 
     parsed_html = Nokogiri::HTML.parse(html)
