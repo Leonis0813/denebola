2bd35db48e103d8890192a3f5a2fb0e02404f806
Leonis0813 <Leonis.0813@gmail.com>
2019-05-13 11:01:23 +0000

    fix parsing html error

--- extract/horse.rb
+++ extract/horse.rb
@@ -1,11 +1,18 @@
 def extract_horse(html)
   {}.tap do |attribute|
     table = html.xpath('//table[contains(@class, "db_h_race_results")]')
-    results = table.search('tbody').children.search('tr').children.search('td')
-    last_race, second_last_race, * = results.map(&:text).map {|text| text.delete("\n") }
+    last_race, second_last_race, * = table.search('tbody').children.search('tr')
+    last_race_order = last_race&.children&.search('td')[11].text.to_i
+    last_race_final_600m_time = last_race&.children&.search('td')[22].text.to_f
+    second_last_race_order = second_last_race&.children&.search('td')[11].text.to_i
 
-    attribute[:last_race_order] = last_race[11] if last_race
-    attribute[:second_last_race_order] = second_last_race[11] if second_last_race
-    attribute[:last_race_final_600m_time] = last_race[22] if last_race
+    attribute[:last_race_order] = last_race_order unless last_race_order == 0
+    unless second_last_race_order == 0
+      attribute[:second_last_race_order] = second_last_race_order
+    end
+    p last_race_final_600m_time
+    unless last_race_final_600m_time == 0.0
+      attribute[:last_race_final_600m_time] = last_race_final_600m_time
+    end
   end
 end
