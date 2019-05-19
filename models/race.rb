0491005e397bb44ffcef6d1f69b49e3f25738cfe
Leonis0813 <Leonis.0813@gmail.com>
2019-05-19 07:15:43 +0000

    validate presence

--- models/race.rb
+++ models/race.rb
@@ -3,7 +3,9 @@
 class Race < ActiveRecord::Base
   has_many :entries
 
+  validates :direction, presence: {message: 'absent'}
   validates :direction, inclusion: {in: %w[左 右], message: 'invalid'}
+  validates :distance, presence: {messege: 'absent'}
   validates :distance,
             numericality: {
               only_integer: true,
@@ -16,19 +18,24 @@ class Race < ActiveRecord::Base
               message: 'invalid',
             },
             allow_nil: true
+  validates :place, presence: {messege: 'absent'}
   validates :place,
             inclusion: {
               in: %w[中京 中山 京都 函館 小倉 新潟 札幌 東京 福島 阪神],
               message: 'invalid',
             }
+  validates :race_id, presence: {messege: 'absent'}
   validates :race_id, format: {with: /\A\d+\z/, message: 'invalid'}
+  validates :round, presence: {messege: 'absent'}
   validates :round,
             numericality: {
               only_integer: true,
               greater_than: 0,
               message: 'invalid',
             }
+  validates :track, presence: {messege: 'absent'}
   validates :track, inclusion: {in: %w[芝 ダ], message: 'invalid'}
+  validates :weather, presence: {messege: 'absent'}
   validates :weather, inclusion: {
               in: %w[晴 曇 小雨 雨 小雪 雪],
               message: 'invalid',
