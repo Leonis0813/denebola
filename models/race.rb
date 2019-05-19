9d857e9519476770f713354a5611a1bb8477cae4
Leonis0813 <Leonis.0813@gmail.com>
2019-05-19 10:05:13 +0000

    refactor race

--- models/race.rb
+++ models/race.rb
@@ -1,46 +1,31 @@
 # coding: utf-8
 
 class Race < ActiveRecord::Base
+  DIRECTION_LIST = %w[左 右]
+  GRADE_LIST = %w[G1 G2 G3 J.G1 J.G2 J.G3 L OP]
+  PLACE_LIST = %w[中京 中山 京都 函館 小倉 新潟 札幌 東京 福島 阪神]
+  TRACK_LIST = %w[芝 ダ]
+  WEATHER_LIST = %w[晴 曇 小雨 雨 小雪 雪]
+
   has_many :entries
 
-  validates :direction, presence: {message: 'absent'}
-  validates :direction, inclusion: {in: %w[左 右], message: 'invalid'}
-  validates :distance, presence: {messege: 'absent'}
-  validates :distance,
-            numericality: {
-              only_integer: true,
-              greater_than: 0,
-              message: 'invalid',
-            }
+  validates :direction, :distance, :place, :race_id, :round, :track, :weather,
+            presence: {message: 'absent'}
+  validates :direction,
+            inclusion: {in: DIRECTION_LIST, message: 'invalid'}
+  validates :distance, :round
+            numericality: {only_integer: true, greater_than: 0, message: 'invalid'}
   validates :grade,
-            inclusion: {
-              in: %w[G1 G2 G3 J.G1 J.G2 J.G3 L OP],
-              message: 'invalid',
-            },
+            inclusion: {in: GRADE_LIST, message: 'invalid'}
             allow_nil: true
-  validates :place, presence: {messege: 'absent'}
   validates :place,
-            inclusion: {
-              in: %w[中京 中山 京都 函館 小倉 新潟 札幌 東京 福島 阪神],
-              message: 'invalid',
-            }
-  validates :race_id, presence: {messege: 'absent'}
-  validates :race_id, format: {with: /\A\d+\z/, message: 'invalid'}
-  validates :round, presence: {messege: 'absent'}
-  validates :round,
-            numericality: {
-              only_integer: true,
-              greater_than: 0,
-              message: 'invalid',
-            }
-  validates :track, presence: {messege: 'absent'}
-  validates :track, inclusion: {in: %w[芝 ダ], message: 'invalid'}
-  validates :weather, presence: {messege: 'absent'}
+            inclusion: {in: PLACE_LIST, message: 'invalid'}
+  validates :race_id,
+            format: {with: /\A\d+\z/, message: 'invalid'}
+  validates :track,
+            inclusion: {in: TRACK_LIST, message: 'invalid'}
   validates :weather,
-            inclusion: {
-              in: %w[晴 曇 小雨 雨 小雪 雪],
-              message: 'invalid',
-            }
+            inclusion: {in: WEATHER_LIST, message: 'invalid'}
 
   def month
     start_time.month
