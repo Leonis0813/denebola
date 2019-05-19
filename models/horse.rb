6f40ae0ba9695facd07323a796d97d126ef5b7ad
Leonis0813 <Leonis.0813@gmail.com>
2019-05-19 10:13:54 +0000

    refactor horse

--- models/horse.rb
+++ models/horse.rb
@@ -1,15 +1,15 @@
 # coding: utf-8
 class Horse < ActiveRecord::Base
+  RUNNING_STYLE_LIST = %w[逃げ 先行 差し 追込]
+
   has_many :results, class_name: 'Entry'
 
-  validates :horse_id, presence: {message: 'absent'}
-  validates :horse_id, format: {with: /\A\d+\z/, message: 'invalid'}
-  validates :running_style, presence: {message: 'absent'}
+  validates :horse_id, :running_style,
+            presence: {message: 'absent'}
+  validates :horse_id,
+            format: {with: /\A\d+\z/, message: 'invalid'}
   validates :running_style,
-            inclusion: {
-              in: %w[逃げ 先行 差し 追込],
-              message: 'invalid',
-            }
+            inclusion: {in: RUNNING_STYLE_LIST, message: 'invalid'}
 
   def average_prize_money(time)
     results_before(time).map(&:prize_money).inject(:+) / entry_times(time).to_f
