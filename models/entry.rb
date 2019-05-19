83b7faece38ecbcee22af66c9eaa2a4e62a3f301
Leonis0813 <Leonis.0813@gmail.com>
2019-05-19 10:10:21 +0000

    refactor entry

--- models/entry.rb
+++ models/entry.rb
@@ -1,56 +1,23 @@
 # coding: utf-8
 class Entry < ActiveRecord::Base
+  SEX_LIST = %w[牝 牡]
+
   belongs_to :race
   belongs_to :horse
 
-  validates :age, presence: {message: 'absent'}
-  validates :age,
-            numericality: {
-              only_integer: true,
-              greater_than: 0,
-              message: 'invalid',
-            }
-  validates :burden_weight, presence: {message: 'absent'}
+  validates :age, :burden_weight, :number, :order, :sex,
+            presence: {message: 'absent'}
+  validates :age, :number, :order,
+            numericality: {only_integer: true, greater_than: 0, message: 'invalid'}
   validates :burden_weight,
-            numericality: {
-              greater_than: 0,
-              message: 'invalid',
-            }
-  validates :final_600m_time,
-            numericality: {
-              greater_than: 0,
-              message: 'invalid',
-            },
+            numericality: {greater_than: 0, message: 'invalid'}
+  validates :final_600m_time, :weight,
+            numericality: {greater_than: 0, message: 'invalid'},
             allow_nil: true
-  validates :number, presence: {message: 'absent'}
-  validates :number,
-            numericality: {
-              only_integer: true,
-              greater_than: 0,
-              message: 'invalid',
-            }
-  validates :order, presence: {message: 'absent'}
-  validates :order,
-            numericality: {
-              only_integer: true,
-              greater_than: 0,
-              message: 'invalid',
-            }
   validates :prize_money,
-            numericality: {
-              only_integer: true,
-              greater_than: 0,
-              message: 'invalid',
-            },
+            numericality: {only_integer: true, greater_than: 0, message: 'invalid'},
             allow_nil: true
-  validates :sex, presence: {message: 'absent'}
-  validates :sex, inclusion: {in: %w[牝 牡], message: 'invalid'}
-  validates :weight,
-            numericality: {
-              greater_than: 0,
-              message: 'invalid',
-            },
-            allow_nil: true
+  validates :sex, inclusion: {in: SEX_LIST, message: 'invalid'}
 
   def weight_per
     return unless burden_weight.to_i.positive? and weight.to_i.positive?
