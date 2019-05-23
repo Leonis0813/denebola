3efc1da44e3b2bf1cab38979acdd75f9cc5834d0
Leonis0813 <Leonis.0813@gmail.com>
2019-05-23 15:15:12 +0000

    output sql

--- db/connect.rb
+++ db/connect.rb
@@ -1,5 +1,7 @@
 require 'active_record'
 require 'mysql2'
 require_relative '../config/initialize'
+require_relative '../lib/denebola_logger'
 
+ActiveRecord::Base.logger = DenebolaLogger.new('log/database.log')
 ActiveRecord::Base.establish_connection(Settings.mysql.to_h)
