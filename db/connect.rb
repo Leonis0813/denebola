0068ac1dcb94c4da2c69a252468743c06fb8bd05
Leonis0813 <Leonis.0813@gmail.com>
2019-06-20 12:21:58 +0000

    fix style

--- db/connect.rb
+++ db/connect.rb
@@ -5,6 +5,6 @@ require_relative '../lib/denebola_logger'
 
 ActiveRecord::Base.logger = DenebolaLogger.new(Settings.logger.path.database)
 ActiveRecord::Base.logger.level = Settings.logger.level.to_sym
-settings = Settings.mysql.map {|key, value| [key.to_s, value] }.to_h.except('database')
-settings.merge!('database' => Settings.mysql.database[ENV['RAILS_ENV']])
+settings = Settings.mysql.map {|key, value| [key.to_s, value] }.to_h
+settings['database'] = Settings.mysql.database[ENV['RAILS_ENV']]
 ActiveRecord::Base.establish_connection(settings)
