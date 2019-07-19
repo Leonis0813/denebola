020163d4a8ce6927d6484ed472bafc86b74bbf49
Leonis <Leonis.0813@gmail.com>
2019-07-19 22:24:18 +0900

    Fix extract entry (#63)
    * fix order
    
    * update CHANGELOG.md
    
    * fix regexp

--- extract/entry.rb
+++ extract/entry.rb
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+require_relative '../models/entry'
+
 def extract_entry(html)
   attributes = html.search('td').map(&:text).map(&:strip)
   horse_link = html.search('td')[3].first_element_child.attribute('href').value
@@ -16,7 +18,7 @@ def extract_entry(html)
     horse_id: horse_link.match(%r{/horse/(?<horse_id>\d+)/?})[:horse_id],
     jockey: attributes[6],
     number: attributes[2].to_i,
-    order: attributes[0].strip[0],
+    order: attributes[0].strip.match(/^(#{Entry::ORDER_LIST.join('|')})$/)[1],
     prize_money: attributes[20].delete(',').to_i * 10000,
     sex: attributes[4].match(/\A([^\d]*)\d+/)[1],
     weight: weight,
