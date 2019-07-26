c9ad5ce59af506f464100e48569f20a860c7a254
Leonis0813 <Leonis.0813@gmail.com>
2019-07-26 12:13:27 +0000

    fix style

--- models/race.rb
+++ models/race.rb
@@ -6,6 +6,8 @@ class Race < ActiveRecord::Base
   PLACE_LIST = %w[中京 中山 京都 函館 小倉 新潟 札幌 東京 福島 阪神].freeze
   TRACK_LIST = %w[芝 ダート 障].freeze
   WEATHER_LIST = %w[晴 曇 小雨 雨 小雪 雪].freeze
+  ONE_PATTERN_TICKET_LIST = %i[win bracket_quinella quinella exacta trio trifecta].freeze
+  MULTI_PATTERNS_TICKET_LIST = %i[shows quinella_places].freeze
 
   has_many :entries
   has_one :win
@@ -39,5 +41,26 @@ class Race < ActiveRecord::Base
 
   def month
     start_time.month
+  end
+
+  def create_payoff(attribute)
+    create_one_pattern_ticket(attribute.slice(*ONE_PATTERN_TICKET_LIST))
+    create_multi_patterns_ticket(attribute.slice(*MULTI_PATTERNS_TICKET_LIST))
+  end
+
+  private
+
+  def create_one_pattern_ticket(attribute)
+    attribute.each do |betting_ticket, attr|
+      send("create_#{betting_ticket}", attr) unless send(betting_ticket)
+    end
+  end
+
+  def create_multi_patterns_ticket(attribute)
+    attribute.each do |betting_ticket, attrs|
+      attrs.each do |attr|
+        send(betting_ticket).find_or_create_by!(attr)
+      end
+    end
   end
 end
