d30015241f9ad19c77b8b5f332214a7dd8b976e1
Leonis0813 <Leonis.0813@gmail.com>
2019-07-28 06:15:43 +0000

    aggregate jockey features

--- models/jockey.rb
+++ models/jockey.rb
@@ -4,4 +4,28 @@ class Jockey < ActiveRecord::Base
   validates :jockey_id,
             presence: {message: 'absent'},
             format: {with: /\A\d+\z/, message: 'invalid'}
+
+  def average_prize_money(time)
+    results_before(time).map(&:prize_money).inject(:+) / entry_times(time).to_f
+  end
+
+  def win_rate(time)
+    win_times(time).to_f / entry_times(time)
+  end
+
+  def win_rate_last_four_races(time)
+    last_four_races = results_before(time).first(4)
+    last_four_races.select(&:won).size.to_f / last_four_races.size
+  end
+
+  private
+
+  def entry_times(time)
+    results_before(time).size
+  end
+
+  def results_before(time)
+    @results_before ||= results.joins(:race).where('races.start_time <= ?', time)
+                               .order('races.start_time desc')
+  end
 end
