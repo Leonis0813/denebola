157d6882ebfce8c0c4fb2c564982bc2469f43a44
Leonis0813 <Leonis.0813@gmail.com>
2019-11-19 11:49:23 +0000

    not null

--- db/migrate/034_add_race_name_to_races.rb
+++ db/migrate/034_add_race_name_to_races.rb
@@ -1,5 +1,5 @@
 class AddRaceNameToRaces < ActiveRecord::Migration[4.2]
   def change
-    add_column :races, :race_name, :string, after: :race_id
+    add_column :races, :race_name, :string, null: false, after: :race_id
   end
 end
